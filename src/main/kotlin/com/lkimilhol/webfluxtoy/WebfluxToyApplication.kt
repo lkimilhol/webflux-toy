4a0bfb3199ae9c341d49a31d9c69d524cac5e26e
lkimilhol <lkimilhol@gmail.com>
2021-11-10 21:39:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/lkimilhol/webfluxtoy/WebfluxToyApplication.kt
@@ -0,0 +1,11 @@
+package com.lkimilhol.webfluxtoy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxToyApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxToyApplication>(*args)
+}
---
+++ src/test/kotlin/com/lkimilhol/webfluxtoy/WebfluxToyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lkimilhol.webfluxtoy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxToyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
